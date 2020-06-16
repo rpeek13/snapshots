a7dfa5e44a7e157965703c7325f1816f62751178
Riley Peek <rileype@amazon.com>
2020-06-16 03:38:20 -0700

    Add setup.py for packaging and distributing the package

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='snapshots-cli',
+    version='0.1',
+    summary='Snapshots-CLI is a tool to manage AWS EC2 snapshots',
+    author='riley peek',
+    author_email='riley.s.peek@gmail.com',
+    license='GPLv3+',
+    packages=['snapshots'],
+    url='https://github.com/rpeek13/snapshots',
+    install_requires=[
+        'click',
+        'boto3'
+    ],
+    entry_points='''
+        [console_scripts]
+        snapshots=snapshots.snapshots:cli
+    ''',
+)
